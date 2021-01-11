6371c1421528cc359661f78d7a1067a3afb27ec8
Noah <aminnnoah19@gmail.com>
2021-01-11 15:45:33 -0600

    fixed config endpoint

---
+++ build/precache-manifest.c68ffbcf38770fc188ff787b8815e618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe312b517ec809b43ac422f9a6f3a5e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1745822b82165f44819",
+    "url": "/static/css/main.41d5af4a.chunk.css"
+  },
+  {
+    "revision": "50e92a0326aa4ef8ffa6",
+    "url": "/static/js/2.64b54b35.chunk.js"
+  },
+  {
+    "revision": "e4ce943d7337e521c77627ac6c85b4f0",
+    "url": "/static/js/2.64b54b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1745822b82165f44819",
+    "url": "/static/js/main.97ccb58e.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  API_ENDPOINT: 'https://damp-temple-58533.herokuapp.com/',
+  API_ENDPOINT: 'https://damp-temple-58533.herokuapp.com',
 };
