07cf7fc5ec7844ce615303a4a82b8857017aee40
Noah <aminnnoah19@gmail.com>
2021-01-12 13:50:42 -0600

    changed endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://damp-temple-58533.herokuapp.com',
+  API_ENDPOINT: 'https://noah-petful-server.herokuapp.com/',
   //API_ENDPOINT: 'http://localhost:8000'
 };
